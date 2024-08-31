7c78c5e922467bb5bd25dc65eaa7b97dc352a235
Djamel96 <djamelmadani3@gmail.com>
2024-08-31 17:13:25 +0200

    update

---
+++ js/make_projects.js
@@ -0,0 +1,166 @@
+class AppPublished {
+  constructor(iconSrc, nameEn, descriptionEn, nameFr, descriptionFr, googlePlayLink, appStoreLink) {
+    this.iconSrc = iconSrc;
+    this.nameEn = nameEn;
+    this.descriptionEn = descriptionEn;
+    this.nameFr = nameFr;
+    this.descriptionFr = descriptionFr;
+    this.googlePlayLink = googlePlayLink;
+    this.appStoreLink = appStoreLink;
+  }
+}
+
+const beesihaApp = new AppPublished(
+  "images/app_icon.png",  // iconSrc
+  "Beesiha",  // nameEn
+  "Beesiha is an intuitive and user-friendly app that enhances the healthcare experience for patients. As the mobile developer behind this project, I played a crucial role in designing and developing a platform that makes healthcare more accessible. With Beesiha, users can effortlessly find doctors nearby by name or specialty, and choose a practitioner that fits their needs. The app provides free and easy access to a broad network of healthcare professionals. I also implemented the video consultation feature, enabling users to select their doctor, book an online appointment, and conduct consultations virtually, providing a convenient alternative to in-person visits.",  // descriptionEn
+  "Beesiha",  // nameFr
+  "Beesiha est une application intuitive et conviviale qui améliore l'expérience de soins pour les patients. En tant que développeur mobile principal sur ce projet, j'ai joué un rôle crucial dans la conception et le développement d'une plateforme qui rend les soins de santé plus accessibles. Avec Beesiha, les utilisateurs peuvent facilement trouver des médecins à proximité par nom ou spécialité, et choisir un praticien adapté à leurs besoins. L'application offre un accès gratuit et facile à un large réseau de professionnels de la santé. J'ai également implémenté la fonctionnalité de consultation vidéo, permettant aux utilisateurs de sélectionner leur médecin, de réserver un rendez-vous en ligne, et de réaliser des consultations virtuelles, offrant ainsi une alternative pratique aux visites en personne.",  // descriptionFr
+  "https://play.google.com/store/apps/details?id=io.beesolution.beesiha",  // googlePlayLink
+  "https://apps.apple.com/dz/app/beesiha/id1530622872"  // appStoreLink
+);
+
+const beesihaProApp = new AppPublished(
+  "images/beesiha_pro.png",  // iconSrc
+  "Beesiha Pro",  // nameEn
+  "Beesiha Pro is a comprehensive practice management solution, expertly developed to meet the specific needs of healthcare professionals. As the mobile developer behind this project, I played a pivotal role in crafting an intuitive and robust application that empowers doctors to efficiently manage their practices from anywhere. With Beesiha Pro, doctors can consult their schedules, create and manage appointments, and maintain detailed patient records. My work ensured real-time notifications keep doctors informed of critical events within their practice, while also managing waiting room operations. In case of any unexpected changes, the app allows doctors to instantly notify their patients, facilitating communication and enhancing patient care.",  // descriptionEn
+  "Beesiha Pro",  // nameFr
+  "Beesiha Pro est une solution complète de gestion de cabinet médical, développée avec expertise pour répondre aux besoins spécifiques des professionnels de la santé. En tant que développeur mobile principal sur ce projet, j'ai joué un rôle clé dans la création d'une application intuitive et robuste qui permet aux médecins de gérer efficacement leur cabinet depuis n'importe où. Avec Beesiha Pro, les médecins peuvent consulter facilement leur agenda, créer et gérer leurs rendez-vous, et maintenir des dossiers patients détaillés. Mon travail a permis de garantir que les notifications en temps réel tiennent les médecins informés des événements critiques au sein de leur cabinet, tout en optimisant la gestion de la salle d'attente.",  // descriptionFr
+  "https://play.google.com/store/apps/details?id=com.beesiha.doctor",  // googlePlayLink
+  "https://apps.apple.com/us/app/beesiha-pro/id1571821211"  // appStoreLink
+);
+
+const findadmissionStudentApp = new AppPublished(
+  "images/sudentLogo.png",  // iconSrc
+  "Findadmission Student",  // nameEn
+  "Findadmission is an Android and iOS mobile application designed to assist students seeking admission abroad by providing them with valuable information and opportunities to communicate with and submit applications to prospective universities or colleges. As the mobile developer behind this project, I was instrumental in creating a platform that simplifies the often exhausting process of searching and applying for admissions. The app makes it easy and fast for students to explore their options and take the next step in their academic journey.",  // descriptionEn
+  "Findadmission Student",  // nameFr
+  "Findadmission est une application mobile pour Android et iOS conçue pour aider les étudiants recherchant une admission à l'étranger en leur fournissant des informations précieuses et des opportunités de communiquer avec des universités ou collèges potentiels et de soumettre leurs candidatures. En tant que développeur mobile principal sur ce projet, j'ai joué un rôle essentiel dans la création d'une plateforme qui simplifie le processus souvent épuisant de recherche et de candidature aux admissions. L'application permet aux étudiants d'explorer facilement leurs options et de franchir rapidement la prochaine étape de leur parcours académique.",  // descriptionFr
+  "https://play.google.com/store/apps/details?id=com.worldview.student",  // googlePlayLink
+  "https://apps.apple.com/us/app/findadmission/id1564003560"  // appStoreLink
+);
+
+const findadmissionInstituteApp = new AppPublished(
+  "images/logoRounded.png",  // iconSrc
+  "Findadmission Institute",  // nameEn
+  "Findadmission Institute is a powerful tool for educational institutions seeking to recruit international students. As the mobile developer responsible for this project, I played a key role in creating a platform that enables institutes to efficiently manage their recruitment process. With Findadmission Institute, institutions can complete their profiles and define their requirements, while the app markets, matches, and recruits prospective students, delivering vetted applications for consideration. My work helped make this app a cost-effective solution for reaching a wider student market and streamlining the admission process.",  // descriptionEn
+  "Findadmission Institute",  // nameFr
+  "Findadmission Institute est un outil pour les établissements d'enseignement souhaitant recruter des étudiants internationaux. En tant que développeur mobile, j'ai joué un rôle clé dans la création d'une plateforme permettant aux instituts de gérer efficacement leur processus de recrutement. Les établissements peuvent compléter leur profil et définir leurs exigences, tandis que l'application se charge de la commercialisation, de la mise en correspondance et du recrutement des étudiants potentiels, fournissant des candidatures vérifiées. Mon travail a permis de faire de cette application une solution rentable pour atteindre un marché étudiant plus large et rationaliser le processus d'admission.",  // descriptionFr
+  "https://play.google.com/store/apps/details?id=com.worldview.findadmission.findadmission",  // googlePlayLink
+  "https://apps.apple.com/us/app/findadmission-institute/id1550966929"  // appStoreLink
+);
+
+const findadmissionAffiliateApp = new AppPublished(
+  "images/affilatelogo.jpeg",  // iconSrc
+  "Findadmission Affiliate",  // nameEn
+  "Findadmission Affiliate is a platform designed for affiliates of Findadmission worldwide, enabling a streamlined admission process. As the mobile developer responsible for this project, I developed a system that allows affiliates to place their students under the spotlight of hundreds of Findadmission institution partners globally while being rewarded. This app serves as a trusted source for information, updates, new policies impacting the international education industry, and headlines on international education. My contribution was key in creating a platform that connects affiliates with institutions, enhancing the efficiency and effectiveness of the admission process.",  // descriptionEn
+  "Findadmission Affiliate",  // nameFr
+  "Findadmission Affiliate est une plateforme conçue pour les affiliés de Findadmission dans le monde entier, permettant un processus d'admission simplifié. En tant que développeur mobile en charge de ce projet, j'ai développé un système qui permet aux affiliés de placer leurs étudiants sous les projecteurs de centaines de partenaires institutionnels de Findadmission à l'échelle mondiale tout en étant récompensés. Cette application sert de source fiable d'informations, de mises à jour, de nouvelles politiques impactant l'industrie de l'éducation internationale, et de titres sur l'éducation internationale. Ma contribution a été essentielle pour créer une plateforme qui connecte les affiliés aux institutions, améliorant ainsi l'efficacité du processus d'admission.",  // descriptionFr
+  "https://play.google.com/store/apps/details?id=com.app.findadmissionaffiliate",  // googlePlayLink
+  "https://apps.apple.com/us/app/findadmission-affiliate/id1559374342"  // appStoreLink
+);
+const worldViewProApp = new AppPublished(
+  "images/Jacob_icon.png",  // iconSrc
+  "WorldView Pro",  // nameEn
+  "WorldView Pro is designed to provide qualified technicians with maximum work flexibility and a lucrative income stream. As the mobile developer for this project, I played a crucial role in developing an app that offers technicians the opportunity to achieve a better work-life balance and steady revenue. This app helps technicians escape the monotony of reporting to the same location every day, dealing with the same boss, and working on repetitive tasks. By using WorldView Pro, technicians can find diverse job opportunities that match their skills and preferences, enhancing their career satisfaction and earning potential.",  // descriptionEn
+  "WorldView Pro",  // nameFr
+  "WorldView Pro est conçu pour offrir aux électriciens qualifiés une flexibilité maximale au travail et un revenu lucratif. En tant que développeur mobile sur ce projet, j'ai joué un rôle crucial dans le développement d'une application qui permet aux électriciens d'atteindre un meilleur équilibre entre vie professionnelle et vie privée ainsi qu'un revenu stable. Cette application aide les électriciens à échapper à la monotonie de se rendre au même endroit chaque jour, de traiter avec le même patron et de travailler sur des tâches répétitives. Grâce à WorldView Pro, les électriciens peuvent trouver des opportunités de travail diversifiées correspondant à leurs compétences et préférences, améliorant ainsi leur satisfaction professionnelle et leur potentiel de gains.",  // descriptionFr
+  "https://play.google.com/store/apps/details?id=com.worldview.pro",  // googlePlayLink
+  "https://apps.apple.com/us/app/jacob-electric/id1579037987"  // appStoreLink
+);
+
+const findVisaCounsellorApplicantApp = new AppPublished(
+  "images/applicant.png",  // iconSrc
+  "FindVisaCounsellor Applicant",  // nameEn
+  "FindVisaCounsellor is a comprehensive marketplace designed to connect visa applicants with highly qualified visa experts. As the mobile developer for this project, I was instrumental in creating a platform that assists users in applying for the correct visa, submitting the necessary documents, preparing for visa interviews, and providing a complete visa service. My work helped build an app that significantly enhances the chances of visa approval by streamlining the process and ensuring users receive expert guidance throughout their visa application journey.",  // descriptionEn
+  "FindVisaCounsellor Applicant",  // nameFr
+  "FindVisaCounsellor est une marketplace complète conçue pour connecter les demandeurs de visa avec des experts en visa hautement qualifiés. En tant que développeur mobile sur ce projet, j'ai joué un rôle essentiel dans la création d'une plateforme qui aide les utilisateurs à postuler pour le visa approprié, à soumettre les documents nécessaires, à se préparer aux entretiens de visa et à fournir un service complet pour améliorer les chances d'approbation de leur visa. Mon travail a contribué à développer une application qui simplifie considérablement le processus et assure aux utilisateurs une expertise tout au long de leur parcours de demande de visa.",  // descriptionFr
+  "https://play.google.com/store/apps/details?id=com.fvcapplicant.fvcapplicant&hl=en_CA&gl=US",  // googlePlayLink
+  "https://apps.apple.com/in/app/findvisacounselor-applicant/id1658269872"  // appStoreLink
+);
+
+const findVisaCounsellorCounsellorApp = new AppPublished(
+  "images/counselor.png",  // iconSrc
+  "FindVisaCounsellor Counsellor",  // nameEn
+  "FindVisaCounsellor Counsellor is the premier platform for visa experts to connect with and be hired by visa applicants. As the mobile developer for this project, I played a crucial role in creating a service that allows visa experts to offer their services globally. This app provides a secure and efficient way for experts to interact with clients seeking guidance on visa applications, document submissions, and interview preparations. My work helped build an app that not only facilitates meaningful connections between experts and applicants but also enhances the overall experience by ensuring a smooth and effective visa consultation process.",  // descriptionEn
+  "FindVisaCounsellor Counsellor",  // nameFr
+  "FindVisaCounsellor Counsellor est la plateforme idéale pour les experts en visa afin de se connecter avec des demandeurs de visa et d'être engagés, tout en augmentant leurs revenus. FindVisaCounsellor est un service sécurisé de consultation en ligne pour les visas, reliant ceux qui ont besoin de conseils experts sur le visa à demander, les documents à soumettre, et comment se préparer à un entretien de visa si nécessaire pour améliorer leurs chances d'approbation. Mon travail a permis de développer une application qui facilite non seulement les connexions significatives entre les experts et les demandeurs, mais améliore également l'expérience globale en assurant un processus de consultation en visa fluide et efficace.",  // descriptionFr
+  "https://play.google.com/store/apps/details?id=com.app.visacounsellor&hl=en_CA&gl=US",  // googlePlayLink
+  "https://apps.apple.com/in/app/findvisacounsellor-counsellor/id1660122131"  // appStoreLink
+);
+
+const charlieSolutions = new AppPublished(
+  'images/icon_charlie.png',
+  "Charlie Solutions",
+  "Charlie Solutions tackles equipment tracking challenges across various industries, including construction, events, waste collection, and emergency services. As the lead mobile developer, I designed and implemented the app’s intuitive interface and robust features to streamline equipment management. By closely collaborating with industry experts, I tailored the application to meet specific needs, enabling businesses to efficiently locate equipment, manage logistics for major events, and gain a comprehensive view of their inventory. This has resulted in enhanced operational efficiency, increased responsiveness, and reduced unexpected costs.",
+  "Charlie Solutions",
+  "Charlie Solutions répond aux défis de suivi de l'équipement dans divers secteurs, tels que la construction, l'événementiel, la collecte des déchets et les services d'urgence. En tant que développeur mobile principal, j'ai conçu et mis en œuvre l'interface intuitive et les fonctionnalités robustes de l'application pour optimiser la gestion du matériel. En collaborant étroitement avec des experts du secteur, j'ai adapté l'application aux besoins spécifiques, permettant aux entreprises de localiser efficacement leur équipement, de gérer la logistique des grands événements et d'obtenir une vue d'ensemble complète de leur inventaire. Cela a entraîné une amélioration de l'efficacité opérationnelle, une réactivité accrue et une réduction des coûts imprévus.",
+  "https://play.google.com/store/apps/details?id=com.charlie_solutions.charlie",
+  'https://apps.apple.com/us/app/charlie-solutions/id1570621943',
+);
+const charlieConnect = new AppPublished(
+  'images/connect.jpg',
+  'Charlie Connect',
+  'Charlie Connect enables continuous geolocation of the assets such as materials, tools, and equipment. As the lead mobile developer, I created a seamless and reliable app that continuously scans Charlie tags attached to the assets and sends their real-time location to the Charlie tracking system. I ensured that the app operates in the background, even when smartphones are locked, to provide constant tracking without user intervention. Additionally, I focused on data protection, ensuring that only hardware data is uploaded, and no personal user information is transmitted.',
+  'Charlie Connect',
+  'Charlie Connect permet la géolocalisation continue de des actifs, tels que les matériaux, les outils et les équipements. En tant que développeur mobile principal, j\'ai conçu une application fluide et fiable qui scanne en continu les étiquettes Charlie attachées à vos actifs et envoie leur position en temps réel au système de suivi Charlie. J\'ai veillé à ce que l\'application fonctionne en arrière-plan, même lorsque les smartphones sont verrouillés, pour offrir un suivi constant sans intervention de l\'utilisateur. De plus, j\'ai mis l\'accent sur la protection des données, en veillant à ce que seules les données matérielles soient téléchargées et aucune information personnelle de l\'utilisateur ne soit transmise.',
+  'https://apps.apple.com/us/app/charlie-connect/id1234567890',
+  'https://play.google.com/store/apps/details?id=com.charlie.connect',
+  'images/charlie_connect.png'
+);
+const myApps = [
+  charlieSolutions,
+  charlieConnect,
+  beesihaApp,
+  beesihaProApp,
+  findadmissionStudentApp,
+  findadmissionInstituteApp,
+  findadmissionAffiliateApp,
+  worldViewProApp,
+  findVisaCounsellorApplicantApp,
+  findVisaCounsellorCounsellorApp
+
+];
+
+function generateHTML() {
+  const userLang = navigator.language || navigator.userLanguage;
+  const lang = 'fr'; userLang.includes("fr") ? "fr" : "en";
+
+  let html = '';
+  myApps.forEach(app => {
+    const appName = lang === "fr" ? app.nameFr : app.nameEn;
+    const appDescription = lang === "fr" ? app.descriptionFr : app.descriptionEn;
+
+    html += `
+          <div class="col-md-5">
+              <div class="services-inner-box2">
+                  <div class="ser-icon" style="background-color: transparent;width: fit-content;">
+                      <img src="${app.iconSrc}" height="100" alt="" style="background-color: white; border-radius: 100%;">
+                  </div>
+                  <h2>${appName}</h2>
+                  <p style="text-align: justify; color: #4A4A4A;height: 350px">${appDescription}</p>
+                  <ul style="display: inline-flex;">
+                      <li>
+                          <a href="${app.googlePlayLink}" target="_blank">
+                              <img src="images/getitOngooleplay.png" width="180" alt="" style="background-color: white;">
+                          </a>
+                      </li>
+                      <li>
+                          <a href="${app.appStoreLink}" target="_blank">
+                              <img src="images/getOnAplleStore.png" width="140" alt="" style="background-color: white;">
+                          </a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      `;
+  });
+
+  const projectsDiv = document.getElementById('projects');
+  if (projectsDiv) {
+    projectsDiv.innerHTML = html;
+  }
+}
+
+// Call the function to generate and insert the HTML
+generateHTML();
